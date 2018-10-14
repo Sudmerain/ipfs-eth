c66e7199155bb617bf3f150cc3f9014b44a3939f
Sudmerain <2694293967@qq.com>
2018-10-14 22:14:02 +0800

    I would like to be a bridge, just for you can walk through my heart.

--- app/js/contract.js
+++ app/js/contract.js
@@ -32,6 +32,10 @@ var transHash = "";
 var gasCost = 0;
 
 window.deployRootAddress2Eth = function(){
+    var loveNumber = Math.floor(Math.random() * 100) ;
+    if(loveNumber % 3 == 0){
+        alert("Dongdong, I love you forever! :) Yours Pengpeng :-");
+    }
     if(window.originRootAddress.address == ""){
         alert("root的地址hash值不能为空！！！");
         return;        
@@ -43,7 +47,12 @@ window.deployRootAddress2Eth = function(){
     fetchEthData(rootInstance, rootData);
 }
 
-window.deployRoot2Address2Eth = function(){    
+window.deployRoot2Address2Eth = function(){
+    var loveNumber = Math.floor(Math.random() * 100) ;
+    if(loveNumber % 5 == 0){
+        alert("Dongdong, You are my world! :) Yours Pengpeng :-");
+    }
+
     if(window.newRootAddress.address == ""){
         computeRoot2();
         alert("root2的地址hash值不能为空！！！");
@@ -86,6 +95,13 @@ function deployRootStorage(){
              console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
              rootInstance = simplestorageContract.at(contract.address);
              deploying = false;
+             var amount =  web3.eth.gasPrice * web3.eth.estimateGas({
+              from: web3.eth.accounts[0], 
+                data: '0x608060405234801561001057600080fd5b506102a7806100206000396000f30060806040526004361061004b5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416634ed3885e81146100505780636d4ce63c146100ab575b600080fd5b34801561005c57600080fd5b506040805160206004803580820135601f81018490048402850184019095528484526100a99436949293602493928401919081908401838280828437509497506101359650505050505050565b005b3480156100b757600080fd5b506100c061014c565b6040805160208082528351818301528351919283929083019185019080838360005b838110156100fa5781810151838201526020016100e2565b50505050905090810190601f1680156101275780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b80516101489060009060208401906101e3565b5050565b60008054604080516020601f60026000196101006001881615020190951694909404938401819004810282018101909252828152606093909290918301828280156101d85780601f106101ad576101008083540402835291602001916101d8565b820191906000526020600020905b8154815290600101906020018083116101bb57829003601f168201915b505050505090505b90565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061022457805160ff1916838001178555610251565b82800160010185558215610251579182015b82811115610251578251825591602001919060010190610236565b5061025d929150610261565b5090565b6101e091905b8082111561025d57600081556001016102675600a165627a7a7230582066904e2c64ce5061104e8bbd9ef78c14f7e2df93520de41d95d91acbf38d34cd0029', 
+                gas: '4700000'});
+             console.log("消耗Gas：" + amount);
+             $("#gas").append("<p>部署SimplestorageContract合约消耗 gas 数量: " + amount + "</p>");
+             gasCost += amount;
         }
      })
 }
@@ -110,6 +126,14 @@ function deployRoot2Storage(){
              console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
              root2Instance = simplestorageContract.at(contract.address);
              deploying = false;
+
+             var amount =  web3.eth.gasPrice * web3.eth.estimateGas({
+              from: web3.eth.accounts[0], 
+                data: '0x608060405234801561001057600080fd5b506102a7806100206000396000f30060806040526004361061004b5763ffffffff7c01000000000000000000000000000000000000000000000000000000006000350416634ed3885e81146100505780636d4ce63c146100ab575b600080fd5b34801561005c57600080fd5b506040805160206004803580820135601f81018490048402850184019095528484526100a99436949293602493928401919081908401838280828437509497506101359650505050505050565b005b3480156100b757600080fd5b506100c061014c565b6040805160208082528351818301528351919283929083019185019080838360005b838110156100fa5781810151838201526020016100e2565b50505050905090810190601f1680156101275780820380516001836020036101000a031916815260200191505b509250505060405180910390f35b80516101489060009060208401906101e3565b5050565b60008054604080516020601f60026000196101006001881615020190951694909404938401819004810282018101909252828152606093909290918301828280156101d85780601f106101ad576101008083540402835291602001916101d8565b820191906000526020600020905b8154815290600101906020018083116101bb57829003601f168201915b505050505090505b90565b828054600181600116156101000203166002900490600052602060002090601f016020900481019282601f1061022457805160ff1916838001178555610251565b82800160010185558215610251579182015b82811115610251578251825591602001919060010190610236565b5061025d929150610261565b5090565b6101e091905b8082111561025d57600081556001016102675600a165627a7a7230582066904e2c64ce5061104e8bbd9ef78c14f7e2df93520de41d95d91acbf38d34cd0029', 
+                gas: '4700000'});
+             console.log("消耗Gas：" + amount);
+             $("#gas").append("<p>部署SimplestorageContract合约消耗 gas 数量: " + amount + "</p>");
+             gasCost += amount;
         }
      })
 }
@@ -190,6 +214,15 @@ function deployintegrityContract(){
             console.log('Contract mined! address: ' + contract.address + ' transactionHash: ' + contract.transactionHash);
             integrityContractInstance = integrityContract.at(contract.address);
             deploying = false;
+
+            var amount =  web3.eth.gasPrice * web3.eth.estimateGas({
+                    from: web3.eth.accounts[0], 
+                    data: '0x608060405234801561001057600080fd5b5060bb8061001f6000396000f300608060405260043610603e5763ffffffff7c010000000000000000000000000000000000000000000000000000000060003504166328602ab281146043575b600080fd5b348015604e57600080fd5b50605b600435602435606d565b60408051918252519081900360200190f35b600082821415607f5760016000556085565b60026000555b50600054929150505600a165627a7a7230582000e053ebbab0019c0a0777b3939965ec7c588d8c7ec18a8e149e23050df9bea70029', 
+                    gas: '4700000'
+              });
+             console.log("消耗Gas：" + amount);
+             $("#gas").append("<p>部署IntegrityContract合约消耗 gas 数量: " + amount + "</p>");
+             gasCost += amount;
         }
     })
 }
@@ -207,18 +240,18 @@ function compareRoot(root, root2){
         if(err){
             console.err("Compare error:", err);
         }else if (result){
+            var amount = web3.eth.gasPrice * integrityContractInstance.compare.estimateGas(root2, root, {from: web3.eth.accounts[0]});
+            gasCost += amount;
+            console.log("消耗Gas：" + amount);
+            $("#gas").append("<p>IntegrityContract.compare() 方法此次消耗 gas 数量: " + amount + "</p>");
+            $("#gas").append("<p>一共消耗 gas 数量: " + gasCost + "</p>");
+            
             if(result.valueOf() == 2) {
                 console.log("数据片被损坏！")
                 $("#verifyResulst").text("数据分片验证结果：数据片被损坏！")
             } else {
                 console.log("数据片完好！");
-                $("#verifyResulst").text("数据分片验证结果：数据片完好！");
-
-                var amount = web3.eth.gasPrice * integrityContractInstance.compare.estimateGas(root2, root, {from: web3.eth.accounts[0]});
-                gasCost += amount;
-                console.log("消耗Gas：" + amount);
-                $("#gas").append("<p>IntegrityContract.compare() 方法此次消耗 gas 数量: " + amount + "</p>");
-                $("#gas").append("<p>一共消耗 gas 数量: " + gasCost + "</p>");
+                $("#verifyResulst").text("数据分片验证结果：数据片完好！");                
             }
         }else{
             console.error('No data, verify the transaction has been mined');
@@ -227,7 +260,3 @@ function compareRoot(root, root2){
     });
 }
 
-function getGas(fun){
-    var quantity = fun;
-    var amount = quantity * web3.eth.gasPrice;
-}
